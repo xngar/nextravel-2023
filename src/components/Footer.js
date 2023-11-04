6b8aae0501fab222d078f14f2036ad5053bd13cd
Mauricio Zuñiga <mantonio.zr@gmail.com>
2023-11-04 00:16:35 -0300

    agregado footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,26 @@
 import React from 'react'
 import "./Footer.css";
+import { BsFacebook, BsInstagram } from "react-icons/bs"
+
 
 const Footer = () => {
     return (
-        <div>Footer</div>
+        <footer>
+            <div>
+                <img src="./img/logo-next-travel.png" />
+            </div>
+            <div>
+                <p>Copyright © 2023 NexTravel S.P.A. All rights reserved | This Site is made with by UP Code E.I.R.L.</p>
+            </div>
+            <div>
+                <div>
+                    <span><BsFacebook /></span>
+                    <span><BsInstagram /></span>
+                </div>
+                <span>E-Mail: contacto@nextravel.cl</span>
+                <span>Celular: 569 92804016</span>
+            </div>
+        </footer>
     )
 }
 
