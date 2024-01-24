52e963a218e3f70473aa7418b77e8dcb89b02d0c
up-code <contacto@upcode.cl>
2024-01-24 00:36:20 -0300

    Slider funcionando mendiante props

--- src/App.js
+++ src/App.js
@@ -6,22 +6,48 @@ import Footer from './components/Footer';
 import { Menu } from './components/Menu';
 import QuienesSomos from './components/QuienesSomos';
 import {Slider} from './components/Slider';
-
-
-import { ContextoDatos } from "./components/contexto/ContextoDatos";
+import {getSlider, obtenerToken, pedirMoneda } from "./components/api/Apis";
+import { useQuery } from "@tanstack/react-query";
 
 function App() {
+const [auth, setAuth] = useState('');
+
+// get Auth / token validation
+const token = useQuery({
+    queryKey: ["user"],
+    queryFn: () => obtenerToken()
+  });
+
+  // pedimos el cambio
+  const cambio = useQuery({
+    queryKey: ["change"],
+    queryFn: () => pedirMoneda(token.data?.value)
+  });
+ 
+  const carousel = useQuery ({
+    queryKey: ["slider"],
+    queryFn: () => getSlider(token.data?.value, 'USD')
+  })
+
+useEffect(()=>{
+     const getToken = localStorage.getItem('token');
+     if(getToken === undefined){
+      setAuth(token.data?.value);
+      localStorage.setItem('token', auth);
+     }
+    
+}, [])
 
  return (
-  <ContextoDatos>
+  <>
       <div className="App">
-        <Menu />
-        <Slider />
+        <Menu change={cambio}/>
+        <Slider items={carousel} />
         <QuienesSomos />
         <Contactenos />
         <Footer />
       </div>
-  </ContextoDatos>
+  </>
   );
 }
 
