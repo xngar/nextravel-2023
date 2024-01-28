bc2c74c83134a6ce25c8c7bdb3d5d6b1a8c37ed4
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-01-28 13:19:36 -0300

    dsd

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import React, { useState } from 'react'
-=======
-import React from 'react';
-import { SelectCurrency } from '../utils/SelectCurrency';
-import { Change } from '../utils/Change';
->>>>>>> 250a6f204bd30f30be21f07e62f2022d615350d6
 import "./Menu.css";
 import { SelectCurrency } from '../utils/SelectCurrency';
 import { Change } from '../utils/Change';
@@ -41,9 +35,9 @@ export const Menu = ({ change }) => {
                     <li>Programas</li>
                     <li>Contacto</li>
                     <li><SelectCurrency /></li>
-                </ul>
-            </div>
+                </ul >
+            </div >
 
-        </nav>
+        </nav >
     )
 }
