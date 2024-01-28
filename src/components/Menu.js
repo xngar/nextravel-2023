00753fb6c7bdb5417436bca6653fd7fffee2a5c0
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-01-28 13:15:20 -0300

    menu para mobile

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,25 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./Menu.css";
 import { SelectCurrency } from '../utils/SelectCurrency';
 import { Change } from '../utils/Change';
+import { GiHamburgerMenu } from "react-icons/gi";
 
 
-export const Menu = ({change}) => {
+export const Menu = ({ change }) => {
+    const [menu, setMenu] = useState(false);
     return (
         <nav className='menu-top' >
             <div className='logo'>
-                <img src="./img/logo-next-travel.png" alt=''/>
+                <img src="./img/logo-next-travel.png" alt='' />
 
             </div>
             <div>
-                <ul>
-                    <Change change={change}/>
+
+                <GiHamburgerMenu className='hambur' onClick={() => setMenu(!menu)} />
+                <ul style={{ visibility: menu ? "visible" : "hidden" }}>
+
+
+                    <Change change={change} />
                     <li>Home</li>
                     <li>¿Quienes somos?</li>
                     <li>Destinos</li>
                     <li>Programas</li>
                     <li>Contacto</li>
-                    <li><SelectCurrency/></li>
+                    <li><SelectCurrency /></li>
                 </ul>
             </div>
 
