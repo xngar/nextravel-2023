e88c18cb60eb1dbe7530c4281c81d757c5c30417
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-01-29 19:59:11 -0300

    trabajando en destinos

--- src/components/Destinos.js
+++ src/components/Destinos.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import "./Destinos.css"
+import { Link } from 'react-router-dom'
 
 const Destinos = () => {
     return (
         <div className='wrapperDestinos'>
             <div className='container'>
-                <figure>
+
+
+                <figure >
                     <img src='./img/mexico-destino.png' />
                     <span>Mexico</span>
                 </figure>
