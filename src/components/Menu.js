fb857d15b441086497d595fbbfc803c390cf1a33
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-02-04 19:12:34 -0300

    merge con mario 04-02-2024

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -1,32 +1,19 @@
-<<<<<<< HEAD
-import React, { useState } from 'react'
-=======
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
->>>>>>> mario
 import "./Menu.css";
 import { SelectCurrency } from '../utils/SelectCurrency';
 import { Change } from '../utils/Change';
 import { GiHamburgerMenu } from "react-icons/gi";
 
 
-<<<<<<< HEAD
 export const Menu = ({ change }) => {
-    const [menu, setMenu] = useState(false);
-    return (
-        <nav className='menu-top' >
-            <div className='logo'>
-                <img src="./img/logo-next-travel.png" alt='' />
-=======
-export const Menu = ({change}) => {
     let uri = document.location.origin;
     return (
         <nav className='menu-top' >
             <div className='logo'>
-            <a href='/'>
-            <img src={`${uri}/img/logo-next-travel.png`} alt=''/>
-            </a>
->>>>>>> mario
+                <a href='/'>
+                    <img src={`${uri}/img/logo-next-travel.png`} alt='' />
+                </a>
 
             </div>
             <div>
@@ -39,16 +26,10 @@ export const Menu = ({change}) => {
                     <li>Destinos</li>
                     <li>Programas</li>
                     <li>Contacto</li>
-<<<<<<< HEAD
-                    <li><SelectCurrency /></li>
-                </ul >
-            </div >
-=======
                     {/* <li><SelectCurrency/></li> */}
                 </ul>
-                 
+
             </div>
->>>>>>> mario
 
         </nav >
     )
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -10,11 +10,32 @@ const formatter = new Intl.NumberFormat('en-US', {
     minimumFractionDigits: 0
 });
 
-<<<<<<< HEAD
-export const Slider = ({ items }) => {
+export const Slider = ({ cambio, items }) => {
+    var url = document.location.origin;
+
+
+    const onClickMe = (i) => {
+
+        var idArea = i.IdArea;
+        var idDestino = i.IdDestino;
+        var idPrograma = i.IdPrograma;
+
+        if (idPrograma != null) {
+            url += `/Programa/${idPrograma}`;
+        } else if (idDestino != null) {
+            url += `/Programas/${idArea}/${idDestino}`;
+        } else if (idArea != null) {
+            url += `/Areas/${idArea}`;
+        }
+
+        window.location.replace(url);
+
+    }
 
     return (<>
+
         <Swiper
+            style={{ cursor: 'pointer' }}
             modules={[Navigation, Pagination, Scrollbar, A11y, EffectFlip, EffectCards, EffectCoverflow, Autoplay]}
             spaceBetween={50}
             slidesPerView={1}
@@ -25,64 +46,17 @@ export const Slider = ({ items }) => {
             }}
             navigation
             scrollbar={{ draggable: true }}
+
         >
             {items.data?.map((item, i) => {
-                return <SwiperSlide key={i} style={{ height: '90vh' }}>
+                return <SwiperSlide key={i} style={{ height: '90vh' }} onClick={() => onClickMe(item)}>
                     <img src={`${process.env.REACT_APP_TURISCLUB_PATH_MEDIA}${item.Src}`} style={{ height: '90vh', width: '100%' }} alt=''></img>
-                    <p>{item && item.ProgramaPrecioTxt} {formatter.format(item.ProgramaPrecioUSD).replace("$", `${CurrencySeleted()} `).replace(",", ".")}</p>
+                    <h3>{item && item.ProgramaPrecioTxt} {formatter.format(item.ProgramaPrecioUSD).replace("$", `${CurrencySeleted()} `).replace(",", ".")}</h3>
+                    <p>valor en pesos Chilenos: {formatter.format(item.ProgramaPrecioUSD * cambio.data.CambioContado).replace(",", ".")}</p>
                 </SwiperSlide>
             })}
         </Swiper>
 
-=======
-export const Slider = ({cambio,items}) => {
-  var url = document.location.origin;
 
- 
-   const onClickMe = (i) =>{
-
-  var idArea = i.IdArea;
-  var idDestino = i.IdDestino;
-  var idPrograma = i.IdPrograma;
-
-  if (idPrograma != null) {
-      url += `/Programa/${idPrograma}`;
-  } else if (idDestino != null) {
-      url += `/Programas/${idArea}/${idDestino}`;
-  } else if (idArea != null){
-      url += `/Areas/${idArea}`;
-  }
-  
-  window.location.replace(url);
-  
-   }
-  
-    return (<>
-  
-   <Swiper
-   style={{cursor:'pointer'}}
-     modules={[Navigation, Pagination, Scrollbar, A11y, EffectFlip, EffectCards, EffectCoverflow, Autoplay]}
-      spaceBetween={50}
-      slidesPerView={1}
-      effect='flip'
-      autoplay={{delay:3000}}
-      pagination={{
-        clickable: true,
-      }}
-      navigation
-      scrollbar={{ draggable: true }}
-      
-      >
-        {items.data?.map((item, i) => {
-             return <SwiperSlide key={i} style={{height:'90vh'}} onClick={()=> onClickMe(item)}>
-                <img src={`${process.env.REACT_APP_TURISCLUB_PATH_MEDIA}${item.Src}`} style={{height:'90vh', width:'100%'}} alt=''></img>
-                <h3>{item && item.ProgramaPrecioTxt} {formatter.format(item.ProgramaPrecioUSD).replace("$", `${CurrencySeleted()} `).replace(",", ".")}</h3>
-                <p>valor en pesos Chilenos: {formatter.format(item.ProgramaPrecioUSD * cambio.data.CambioContado).replace(",", ".")}</p>
-             </SwiperSlide>
-        })}
-     </Swiper>
-
-    
->>>>>>> mario
     </>)
 }
