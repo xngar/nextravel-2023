a02d0263f10c826339fb20d0325f982eb14b6b44
Mauricio Zuñiga <mantonio.zr@gmail.com>
2024-02-04 19:06:39 -0300

    avances antonio domingo 04

--- src/components/Destinos.js
+++ src/components/Destinos.js
@@ -1,32 +1,43 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import "./Destinos.css"
 import { Link } from 'react-router-dom'
+import userEvent from '@testing-library/user-event'
 
 const Destinos = () => {
+
+    const figura = useRef(null);
+    const figura2 = useRef(null);
+
+
+    function figuraAnim(figura) {
+        figura.current.classList.add('activo');
+    }
+
+    function figuraRemove(figura) {
+        figura.current.classList.remove('activo');
+    }
+
     return (
         <div className='wrapperDestinos'>
             <div className='container'>
 
 
-                <figure >
+
+
+                <figure onMouseEnter={() => figuraAnim(figura)} onMouseLeave={() => figuraRemove(figura)} ref={figura} >
+                    <div></div>
                     <img src='./img/mexico-destino.png' />
                     <span>Mexico</span>
                 </figure>
 
-                <figure>
+                <figure onMouseEnter={() => figuraAnim(figura2)} onMouseLeave={() => figuraRemove(figura2)} ref={figura2} >
+                    <div></div>
                     <img src='./img/mexico-destino.png' />
                     <span>Mexico</span>
                 </figure>
 
-                <figure>
-                    <img src='./img/mexico-destino.png' />
-                    <span>Mexico</span>
-                </figure>
 
-                <figure>
-                    <img src='./img/mexico-destino.png' />
-                    <span>Mexico</span>
-                </figure>
+
 
             </div>
         </div>
